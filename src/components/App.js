94d94f8015f9d4bd476a3f8d6a1aa60addbf24a0
nataliaventura93 <nataliaventura93@gmail.com>
2023-06-26 09:05:50 +0200

    Add html

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,40 @@ function App() {
     Variables de estado, funciones manejadoras de eventos, variables, funcion handle 
   */
   /* RETURN --> HTML */
-  return <div>pagina principal</div>;
+  return (
+    <div>
+      <h1>Frases de Friends</h1>
+      <form>
+        <label>Filtrar por frase</label>
+        <input type="text" />
+        <label>Filtrar por personaje</label>
+        <select name="select">
+          <option value="all" selected>
+            Todos
+          </option>
+          <option value="R">Ross</option>
+          <option value="M">Monica</option>
+          <option value="M">Joey</option>
+          <option value="M">Phoebe</option>
+          <option value="M">Chandler</option>
+          <option value="M">Rachel</option>
+        </select>
+      </form>
+      <ul>
+        <li>
+          <p>Frase<span>Personaje</span></p>
+        </li>
+      </ul>
+      <form>
+        <h2>Añadir una nueva frase</h2>
+        <label htmlFor="">Frase</label>
+        <input type="text" />
+        <label htmlFor="">Personaje</label>
+        <input type="text" />
+        <input type="submit" value="Añadir"/>
+      </form>
+    </div>
+  );
 }
 
-/* export*/
 export default App;
